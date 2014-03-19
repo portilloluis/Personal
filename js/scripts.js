e7ff01b3e3915239ed79c5d4e23bc7f289493fba
Luis Portillo <lcpdiseno@gmail.com>
2014-03-18 23:16:40 -0600

    Primera carga

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+//Script para abrir y cerrar recuadros de información en los proyectos.
+
+//Mardueño Ritz & Asociados
+$( "#abrir_pro_mardueno" ).click(function() {
+  $( "#texto_pro_mardueno" ).toggle( "slow", function() {
+  });
+});
+
+//Digital Plus
+$( "#abrir_pro_digital_plus" ).click(function() {
+  $( "#texto_pro_digital_plus" ).toggle( "slow", function() {
+  });
+});
+
+//High Preformance States
+$( "#abrir_pro_high_preformance" ).click(function() {
+  $( "#texto_pro_high_preformance" ).toggle( "slow", function() {
+  });
+});
+
+//iShow
+$( "#abrir_pro_ishow" ).click(function() {
+  $( "#texto_pro_ishow" ).toggle( "slow", function() {
+  });
+});
+
+//Casa Morales
+$( "#abrir_pro_casa_morales" ).click(function() {
+  $( "#texto_pro_casa_morales" ).toggle( "slow", function() {
+  });
+});
+
+//Cassa Inova
+$( "#abrir_pro_cassa_inova" ).click(function() {
+  $( "#texto_pro_cassa_inova" ).toggle( "slow", function() {
+  });
+});
+
+//Sanborns
+$( "#abrir_pro_sanborns" ).click(function() {
+  $( "#texto_pro_sanborns" ).toggle( "slow", function() {
+  });
+});
+
+//Festival Yucatán Sanborns
+$( "#abrir_pro_festival_yucatan" ).click(function() {
+  $( "#texto_pro_festival_yucatan" ).toggle( "slow", function() {
+  });
+});
+
+//Membranas la Marina
+$( "#abrir_pro_membranas" ).click(function() {
+  $( "#texto_pro_membranas" ).toggle( "slow", function() {
+  });
+});
---
+++ js/vendor/fancybox_proyectos.js
@@ -0,0 +1,123 @@
+$(document).ready(function() {			
+				
+				$("#pro_digital_plus").click(function() {
+					$.fancybox.open([
+						{
+							href : 'img/proyectos/digital_plus/digital_plus_1.jpg',
+							title : 'Papelería corporativa'
+						}, {
+							href : 'img/proyectos/digital_plus/digital_plus_2.jpg',
+							title : 'Hoja Membretada'
+						}, {
+							href : 'img/proyectos/digital_plus/digital_plus_3.jpg',
+							title : 'Tarjeta de presentación'
+						}, {
+							href : 'img/proyectos/digital_plus/digital_plus_4.jpg',
+							title : 'Sitio web'
+						}, {
+							href : 'img/proyectos/digital_plus/digital_plus_5.jpg',
+							title : 'Bolsa promocional'
+						}
+					]);
+				});
+				$("#pro_ishow").click(function() {
+					$.fancybox.open([
+						{
+							href : 'img/proyectos/ishow/ishow_1.jpg',
+							title : 'Tarjeta de presentación'
+						}, {
+							href : 'img/proyectos/ishow/ishow_2.jpg',
+							title : 'Hoja Membretada'
+						}, {
+							href : 'img/proyectos/ishow/ishow_3.jpg',
+							title : 'Postales'
+						}
+					]);
+				});
+				$("#pro_casa_morales").click(function() {
+					$.fancybox.open([
+						{
+							href : 'img/proyectos/casa_morales/casa_morales_1.jpg',
+							title : 'Imagen corporativa'
+						}, {
+							href : 'img/proyectos/casa_morales/casa_morales_2.jpg',
+							title : 'Tarjeta de presentación'
+						}, {
+							href : 'img/proyectos/casa_morales/casa_morales_3.jpg',
+							title : 'Hoja membretada'
+						}, {
+							href : 'img/proyectos/casa_morales/casa_morales_4.jpg',
+							title : 'Firma digital'
+						}, {
+							href : 'img/proyectos/casa_morales/casa_morales_5.jpg',
+							title : 'Uniformes para caballero'
+						}, {
+							href : 'img/proyectos/casa_morales/casa_morales_6.jpg',
+							title : 'Uniformes para damas'
+						}
+					]);
+				});
+
+				$("#pro_sanborns").click(function() {
+					$.fancybox.open([
+						{
+							href : 'img/proyectos/sanborns/sanborns_1.jpg',
+							title : 'Propuesta Vacaciones'
+						}, {
+							href : 'img/proyectos/sanborns/sanborns_2.jpg',
+							title : 'Promoción audífonos Monster'
+						}, {
+							href : 'img/proyectos/sanborns/sanborns_3.jpg',
+							title : 'Propuesta diseño topten'
+						}, {
+							href : 'img/proyectos/sanborns/sanborns_4.jpg',
+							title : 'Menú festival del sope'
+						}, {
+							href : 'img/proyectos/sanborns/sanborns_5.jpg',
+							title : 'Propuesta portada catálogo anual'
+						}, {
+							href : 'img/proyectos/sanborns/sanborns_6.jpg',
+							title : 'Promoción Ipod y Ipad'
+						}, {
+							href : 'img/proyectos/sanborns/sanborns_7.jpg',
+							title : 'Tarjeta monedero revistas digitales'
+						}
+					]);
+				});
+
+				$("#pro_festival_yucatan").click(function() {
+					$.fancybox.open([
+						{
+							href : 'img/proyectos/festival_yucatan/festival_yucatan_1.jpg',
+							title : 'Imagotipo'
+						}, {
+							href : 'img/proyectos/festival_yucatan/festival_yucatan_2.jpg',
+							title : 'Cartel comidas'
+						}, {
+							href : 'img/proyectos/festival_yucatan/festival_yucatan_3.jpg',
+							title : 'Tentcard cenas'
+						}, {
+							href : 'img/proyectos/festival_yucatan/festival_yucatan_4.jpg',
+							title : 'Imagen redes sociales'
+						}
+					]);
+				});
+
+				$("#pro_cassa_inova").click(function() {
+					$.fancybox.open([
+						{
+							href : 'img/proyectos/cassa_inova/cassa_inova_1.jpg',
+							title : 'Imagotipo'
+						}, {
+							href : 'img/proyectos/cassa_inova/cassa_inova_2.jpg',
+							title : 'Volantes promocionales'
+						}, {
+							href : 'img/proyectos/cassa_inova/cassa_inova_3.jpg',
+							title : 'Hoja membretada'
+						}, {
+							href : 'img/proyectos/cassa_inova/cassa_inova_4.jpg',
+							title : 'Tarjeta de presentación'
+						}
+					]);
+				});
+			});
